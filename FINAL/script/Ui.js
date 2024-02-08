7c3c4792aa3c18bbe183e1b545e15fe3ed2b72b7
Fadil-Tao <hadadfadilah2354@gmail.com>
2024-02-08 14:41:53 +0800

    bwah

---
+++ FINAL/script/Ui.js
@@ -0,0 +1,50 @@
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+
+  elmnt.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+  // Select all input elements within the ".inputs" container
+  const inputs = document.querySelectorAll(".inputs input");
+
+  // Loop through each input and add an event listener to stop event propagation
+  inputs.forEach((input) => {
+    input.addEventListener("mousedown", function (e) {
+      e.stopPropagation(); // Stop the propagation of the mousedown event
+    });
+  });
+}
+
+const inputUi = document.getElementById("inputUi");
+dragElement(inputUi);
--- FINAL/script/script.js
+++ FINAL/script/script.js
@@ -4,132 +4,169 @@ let imgData = ctx.createImageData(1, 1);
 let CanvasMiddleX = CANVAS.width / 2;
 let CanvasMiddleY = CANVAS.height / 2;
 
-// Input
-let tinggiBendaInput = 100;
-let jarakBendaInput = 250;
-let jarakFokusInput = 100;
+let TinggiObjek = document.getElementById("tinggiObjek");
+let tinggiObjekValue = document.getElementById("tinggiObjekValue");
+tinggiObjekValue.textContent = TinggiObjek.value;
+let JarakBenda = document.getElementById("jarakBenda");
+let jarakBendaValue = document.getElementById("jarakBendaValue");
+jarakBendaValue.textContent = JarakBenda.value;
+let JarakFokus = document.getElementById("jarakFokus");
+let jarakFokusValue = document.getElementById("jarakFokusValue");
+jarakFokusValue.textContent = jarakFokus.value;
 
+// Input
+let tinggiBendaInput = TinggiObjek.value;
+let jarakBendaInput = JarakBenda.value;
+let jarakFokusInput = JarakFokus.value;
+
+TinggiObjek.addEventListener("input", () => {
+  tinggiBendaInput = TinggiObjek.value;
+  tinggiObjekValue.textContent = TinggiObjek.value;
+});
+
+JarakBenda.addEventListener("input", () => {
+  jarakBendaInput = JarakBenda.value;
+  jarakBendaValue.textContent = JarakBenda.value;
+});
+
+JarakFokus.addEventListener("input", () => {
+  jarakFokusInput = JarakFokus.value;
+  jarakFokusValue.textContent = JarakFokus.value;
+});
+
+console.log(tinggiBendaInput);
 // Real Input Kordinat
-const titikFokus = {
-    "x": CanvasMiddleX - jarakFokusInput,
-    "y": CanvasMiddleY
-}
-const tinggiBenda = {
-    "x": CanvasMiddleX - jarakBendaInput,
-    "y": CanvasMiddleY - tinggiBendaInput
-}
-const jarakBenda = {
-    "x": CanvasMiddleX - jarakBendaInput,
-    "y": CanvasMiddleY
-}
+let titikFokus = {
+  x: CanvasMiddleX - jarakFokusInput,
+  y: CanvasMiddleY,
+};
+let tinggiBenda = {
+  x: CanvasMiddleX - jarakBendaInput,
+  y: CanvasMiddleY - tinggiBendaInput,
+};
+let jarakBenda = {
+  x: CanvasMiddleX - jarakBendaInput,
+  y: CanvasMiddleY,
+};
 
 function drawPixel(x, y, colour = "black") {
-    if (colour == "blue") {
-        ctx.fillStyle = "blue";
-    } else if (colour == "red") {
-        ctx.fillStyle = "red";
-    } else if (colour == "yellow") {
-        ctx.fillStyle = "yellow";
-    } else if (colour == "green") {
-        ctx.fillStyle = "green";
-    } else {
-        ctx.fillStyle = "black";
-    }
-    ctx.fillRect(x, y, 1, 1);
+  if (colour == "blue") {
+    ctx.fillStyle = "blue";
+  } else if (colour == "red") {
+    ctx.fillStyle = "red";
+  } else if (colour == "yellow") {
+    ctx.fillStyle = "yellow";
+  } else if (colour == "green") {
+    ctx.fillStyle = "green";
+  } else {
+    ctx.fillStyle = "black";
+  }
+  ctx.fillRect(x, y, 1, 1);
 }
 
 function garisDDA(x1, y1, x2, y2, colour) {
-    let titikx_awal = x1;
-    let titiky_awal = y1;
+  let titikx_awal = x1;
+  let titiky_awal = y1;
 
-    let titikx_akhir = x2;
-    let titiky_akhir = y2;
+  let titikx_akhir = x2;
+  let titiky_akhir = y2;
 
-    let dx = titikx_akhir - titikx_awal;
-    let dy = titiky_akhir - titiky_awal;
-    let step = 0;
+  let dx = titikx_akhir - titikx_awal;
+  let dy = titiky_akhir - titiky_awal;
+  let step = 0;
 
-    if (Math.abs(dx) > Math.abs(dy)) {
-        step = dx;
-    } else {
-        step = dy;
-    }
+  if (Math.abs(dx) > Math.abs(dy)) {
+    step = dx;
+  } else {
+    step = dy;
+  }
 
-    let x_inc = dx / Math.abs(step);
-    let y_inc = dy / Math.abs(step);
+  let x_inc = dx / Math.abs(step);
+  let y_inc = dy / Math.abs(step);
 
-    let x = titikx_awal;
-    let y = titiky_awal;
+  let x = titikx_awal;
+  let y = titiky_awal;
 
-    for (let s = 0; s < step; s += 1) {
-        drawPixel(x, y, colour);
-        x = x + x_inc;
-        xa = Math.ceil(x);
-        y = y + y_inc;
-        ya = Math.ceil(y);
-        ctx.putImageData(imgData, xa, ya);
-    }
+  for (let s = 0; s < step; s += 1) {
+    drawPixel(x, y, colour);
+    x = x + x_inc;
+    let xa = Math.ceil(x);
+    y = y + y_inc;
+    let ya = Math.ceil(y);
+    ctx.putImageData(imgData, xa, ya);
+  }
 }
 
 function getY(x1, y1, x2, y2, xi) {
-    const dy = y2 - y1;
-    const dx = x2 - x1;
-    const m = dy / dx;
-    const c = y1 - m * x1;
+  let dy = y2 - y1;
+  let dx = x2 - x1;
+  let m = dy / dx;
+  let c = y1 - m * x1;
 
-    return m * xi + c;
+  return m * xi + c;
 }
 
 function getX(x1, y1, x2, y2, yi) {
-    const dy = y2 - y1;
-    const dx = x2 - x1;
-    const m = dy / dx;
-    const c = y1 - m * x1;
+  let dy = y2 - y1;
+  let dx = x2 - x1;
+  let m = dy / dx;
+  let c = y1 - m * x1;
 
-    return (yi - c) / m;
+  return (yi - c) / m;
 }
 
 function MainCrossline() {
-    garisDDA(CanvasMiddleX, 0, CanvasMiddleX, CANVAS.height, "black");
-    garisDDA(0, CanvasMiddleY, CANVAS.width, CanvasMiddleY, "black");
+  garisDDA(CanvasMiddleX, 0, CanvasMiddleX, CANVAS.height, "black");
+  garisDDA(0, CanvasMiddleY, CANVAS.width, CanvasMiddleY, "black");
 }
 
 function initInput() {
-    // Jarak Fokus
-    garisDDA(titikFokus.x, titikFokus.y - 3, titikFokus.x, titikFokus.y + 3, "red");
-    // Benda
-    garisDDA(tinggiBenda.x, tinggiBenda.y, jarakBenda.x, jarakBenda.y + 1, "blue");
+  // Jarak Fokus
+  garisDDA(
+    titikFokus.x,
+    titikFokus.y - 3,
+    titikFokus.x,
+    titikFokus.y + 3,
+    "red"
+  );
+  // Benda
+  garisDDA(
+    tinggiBenda.x,
+    tinggiBenda.y,
+    jarakBenda.x,
+    jarakBenda.y + 1,
+    "blue"
+  );
 }
 
 function cahayaDatang() {
-    // Merah
-    garisDDA(
-        0,
-        tinggiBenda.y,
-        CanvasMiddleX,
-        tinggiBenda.y,
-        "red"
-    );
-    // Kuning
-    garisDDA(
-        0,
-        getY(tinggiBenda.x, tinggiBenda.y, CanvasMiddleX, CanvasMiddleY, 0),
-        CanvasMiddleX,
-        CanvasMiddleY,
-        "yellow"
-    );
-    // Hijau
-    garisDDA(
-        getX(tinggiBenda.x, tinggiBenda.y, titikFokus.x, titikFokus.y, 0),
-        0,
-        CanvasMiddleX,
-        getY(tinggiBenda.x, tinggiBenda.y, titikFokus.x, titikFokus.y, CanvasMiddleX),
-        "green"
-    );
+  // Merah
+  garisDDA(0, tinggiBenda.y, CanvasMiddleX, tinggiBenda.y, "red");
+  // Kuning
+  garisDDA(
+    0,
+    getY(tinggiBenda.x, tinggiBenda.y, CanvasMiddleX, CanvasMiddleY, 0),
+    CanvasMiddleX,
+    CanvasMiddleY,
+    "yellow"
+  );
+  // Hijau
+  garisDDA(
+    getX(tinggiBenda.x, tinggiBenda.y, titikFokus.x, titikFokus.y, 0),
+    0,
+    CanvasMiddleX,
+    getY(
+      tinggiBenda.x,
+      tinggiBenda.y,
+      titikFokus.x,
+      titikFokus.y,
+      CanvasMiddleX
+    ),
+    "green"
+  );
 }
 
 // Run
 MainCrossline();
 cahayaDatang();
 initInput();
-
